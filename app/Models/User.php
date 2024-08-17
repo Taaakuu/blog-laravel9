ffab7c69d293734bc3957d5584f9b893f9a9c308
Taaakuu <fujiwarachris324@gmail.com>
2024-08-17 14:10:02 +0900

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -66,4 +67,16 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "https://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    /**
+     * 一个用户拥有多条微博
+     *
+     * @return HasMany
+     */
+
+    public function statuses(): HasMany
+    {
+        return $this->hasMany(Status::class);
+    }
+
 }
