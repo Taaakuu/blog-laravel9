37c9de0912b09ccd4da2543a2a7f55ee40b4f5ed
Taaakuu <fujiwarachris324@gmail.com>
2024-08-20 00:42:23 +0900

    社交信息统计

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 @extends('layouts.default')
 
 @section('content')
@@ -14,6 +15,9 @@
             <aside class="col-md-4">
                 <section class="user_info">
                     @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+                <section class="stats mt-2">
+                    @include('shared._stats', ['user' => Auth::user()])
                 </section>
             </aside>
         </div>
